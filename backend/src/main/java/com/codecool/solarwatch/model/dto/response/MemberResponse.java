99a1521f088bdbe9c5016ac9961b6701e83ff852
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-02 12:41:27 +0100

    add MemberResponse dto for authentication

--- backend/src/main/java/com/codecool/solarwatch/model/dto/response/MemberResponse.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/response/MemberResponse.java
@@ -1,4 +1,13 @@
 package com.codecool.solarwatch.model.dto.response;
 
+import com.codecool.solarwatch.model.entity.Role;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class MemberResponse {
+    private String username;
+    private String password;
+    private Set<String> roles;
 }
