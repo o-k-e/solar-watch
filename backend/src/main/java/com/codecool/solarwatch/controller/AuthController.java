e8023f5ef6471417696d94a6659c44393b69d9b3
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-02 12:43:55 +0100

    rename MemberController to AuthController

--- backend/src/main/java/com/codecool/solarwatch/controller/AuthController.java
+++ backend/src/main/java/com/codecool/solarwatch/controller/AuthController.java
@@ -5,25 +5,18 @@ import com.codecool.solarwatch.model.dto.response.JwtResponse;
 import com.codecool.solarwatch.model.dto.response.MemberResponse;
 import com.codecool.solarwatch.security.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/user")
-public class MemberController {
+public class AuthController {
 
     private final AuthenticationService authenticationService;
-//    private final PasswordEncoder encoder;
-//    private final AuthenticationManager authenticationManager;
-//    private final JwtUtils jwtUtils;
 
 
-    public MemberController(AuthenticationService authenticationService) {
+    public AuthController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
-//        this.encoder = encoder;
-//        this.authenticationManager = authenticationManager;
-//        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/register")
