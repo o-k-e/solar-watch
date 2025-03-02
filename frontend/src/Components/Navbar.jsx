36775f048a529faff4f0dbda81b2571f9f6d63f4
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-02 12:31:03 +0100

    create Layout and Navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Routes from './Routes';
+
+const Navbar = () => {
+  return (
+    <div className='text-left text-[#ff8c00] text-3xl p-10 pb-1 bg-black'>Navbar</div>
+  )
+}
+
+export default Navbar
