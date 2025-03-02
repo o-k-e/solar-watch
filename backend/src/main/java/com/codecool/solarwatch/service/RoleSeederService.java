55403aef852c7451d2a7bc5e115fa48d3077ade8
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-02 12:38:48 +0100

    update RoleSeederService to add default admin to db

--- backend/src/main/java/com/codecool/solarwatch/service/RoleSeederService.java
+++ backend/src/main/java/com/codecool/solarwatch/service/RoleSeederService.java
@@ -1,31 +1,47 @@
 package com.codecool.solarwatch.service;
 
+import com.codecool.solarwatch.model.entity.Member;
 import com.codecool.solarwatch.model.entity.Role;
+import com.codecool.solarwatch.repository.MemberRepository;
 import com.codecool.solarwatch.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class RoleSeederService {
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public RoleSeederService(RoleRepository roleRepository) {
+
+    public RoleSeederService(RoleRepository roleRepository, MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
     public void createRoles() {
 
-        if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+        if (roleRepository.findByName("USER").isEmpty()) {
             Role role = new Role();
-            role.setName("ROLE_USER");
+            role.setName("USER");
             roleRepository.save(role);
         }
 
-        if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+        if (roleRepository.findByName("ADMIN").isEmpty()) {
             Role role = new Role();
-            role.setName("ROLE_ADMIN");
+            role.setName("ADMIN");
             roleRepository.save(role);
+
+            Member member = new Member();
+            member.setUsername("admin");
+            member.setPassword(passwordEncoder.encode("admin"));
+            member.setRoles(Set.of(role));
+            memberRepository.save(member);
         }
     }
 }
