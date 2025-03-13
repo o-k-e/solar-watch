de3418e54479f16f2e85f75989a8a966f927ce5a
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-03-13 15:37:01 +0100

    add @NoArgs @AllArgs annotations

--- backend/src/main/java/com/codecool/solarwatch/model/dto/request/MemberRequest.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/request/MemberRequest.java
@@ -1,9 +1,14 @@
 package com.codecool.solarwatch.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberRequest {
     private String username;
     private String password;
+
 }
--- backend/src/main/java/com/codecool/solarwatch/model/entity/Member.java
+++ backend/src/main/java/com/codecool/solarwatch/model/entity/Member.java
@@ -1,13 +1,17 @@
 package com.codecool.solarwatch.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     @Id
