d7267e7f472db4ddf843eaea7e542083a8f04459
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-20 23:01:56 +0200

    change bg color and text color

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-900 text-white px-4">
+        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-[#272121] text-white px-4">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to SolarWatch</h1>
             <p className="text-lg md:text-xl mb-8">Track sunrise and sunset times by city 🌅 </p>
             <div className="flex gap-4">
-                <Link to="/login" className="bg-white text-black px-6 py-2 rounded shadow hover:bg-gray-200 transition">Login</Link>
-                <Link to="/register" className="border border-white px-6 py-2 rounded hover:bg-white hover:text-black transition">Register</Link>
+                <Link to="/login" className="bg-white text-[#272121] px-6 py-2 rounded shadow hover:bg-gray-200 transition">Login</Link>
+                <Link to="/register" className="border border-white px-6 py-2 rounded hover:bg-white hover:text-[#272121] transition">Register</Link>
             </div>
         </div>
     );
