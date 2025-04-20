fdb0c94799473f8fa94a2ad322a0f8bea68c28d4
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-20 23:02:14 +0200

    change bg color

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ import ROUTES from './Routes';
 
 const Navbar = () => {
   return (
-    <div className='text-left text-[#ff8c00] text-3xl p-10 pb-1 bg-gray-900'>SolarWatch</div>
+    <div className='text-left text-[#ff8c00] text-3xl p-10 pb-1 bg-[#272121]'>SolarWatch</div>
   )
 }
 
-export default Navbar
+export default Navbar;
