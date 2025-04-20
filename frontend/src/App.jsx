d2c43f057f95ffc186f67233ded1c4651a8124ee
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-20 23:01:28 +0200

    comment out whole file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-        <h1 className="text-3xl font-bold">Solar Watch</h1>
-
-    </>
-  )
-}
-
-export default App;
+// import './App.css'
+//
+// function App() {
+//
+//   return (
+//     <>
+//         <h1 className="text-3xl font-bold">Solar Watch</h1>
+//
+//     </>
+//   )
+// }
+//
+// export default App;
