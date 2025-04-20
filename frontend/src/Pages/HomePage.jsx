7066448a8693fc797e405027866f2a3cd55913e4
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-20 23:02:32 +0200

    create HomePage.jsx

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+
+    return (
+        <>
+            <div className="flex justify-center items-center min-h-screen bg-gray-100">
+                <h1>Welcome to the Solar Watch Home Page!</h1>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
