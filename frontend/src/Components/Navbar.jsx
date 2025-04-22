e4f2cee782fd5dbe3682cd91b26a26a8f3b81dc3
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-22 12:59:50 +0200

    fix padding

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import ROUTES from './Routes';
 
 const Navbar = () => {
   return (
-    <div className='text-left text-[#ff8c00] text-3xl p-10 pb-1 bg-[#272121]'>SolarWatch</div>
+    <div className='text-left text-[#ff8c00] text-3xl p-10 bg-[#272121]'>SolarWatch</div>
   )
 }
 
