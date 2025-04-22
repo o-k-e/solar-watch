5d16db226ff685a982dfbbbfd17c89317823fb75
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-23 00:41:50 +0200

    add logout link to navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import ROUTES from './Routes';
+import {useNavigate} from "react-router-dom";
+import {checkLoginStatus, logout} from "../Service/AuthService.js";
 
 const Navbar = () => {
-  return (
-    <div className='text-left text-[#ff8c00] text-3xl p-10 bg-[#272121]'>SolarWatch</div>
+
+    const navigate = useNavigate();
+    const isLoggedIn = checkLoginStatus();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    }
+
+    return (
+    <div className='text-left text-[#ff8c00] text-3xl p-10 bg-[#272121] flex justify-between items-center'>
+        <span>SolarWatch</span>
+        {isLoggedIn && (
+            <button
+                onClick={handleLogout}
+                className="text-[#ff8c00]"
+            >
+                Logout
+            </button>
+        )}
+
+    </div>
   )
 }
 
