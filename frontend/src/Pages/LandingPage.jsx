177b432ae33e5a6a894f5e7b506d6315278ba2cf
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-23 18:03:11 +0200

    modify background

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-[#1e3163] text-white px-4">
+        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-[#091930] text-white px-4">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to SolarWatch</h1>
             <p className="text-lg md:text-xl mb-8">Track sunrise and sunset times by city 🌅 </p>
             <div className="flex gap-4">
