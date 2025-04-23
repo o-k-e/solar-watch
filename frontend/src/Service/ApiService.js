a2d9749d91069d0e9c338bb6e8858d64fbfc680d
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-23 18:10:11 +0200

    add fetchAllCities function

--- frontend/src/Service/ApiService.js
+++ frontend/src/Service/ApiService.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {options} from "axios";
 
 export const fetchSolarData = async (cityName, date) => {
     const jwt = localStorage.getItem("jwt");
@@ -18,3 +18,23 @@ export const fetchSolarData = async (cityName, date) => {
         throw error;
     }
 };
+
+
+export const fetchAllCities = async () => {
+    const jwt = localStorage.getItem("jwt");
+
+    const options = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${jwt}`,
+        }
+    }
+
+    try {
+        const response = await axios.get(`http://localhost:8080/city/find-all`, options);
+        return response.data;
+    } catch (error) {
+        console.error("Error in fetching all cities", error);
+        throw error;
+    }
+}
