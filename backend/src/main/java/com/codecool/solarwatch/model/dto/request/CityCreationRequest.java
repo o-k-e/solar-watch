3efd9b855df9f8a47d42f6248260de1d5af5f7b4
o-k-e <123556323+o-k-e@users.noreply.github.com>
2025-04-29 13:00:22 +0200

    refactor(dto): convert DTO classes to records for consistency

--- backend/src/main/java/com/codecool/solarwatch/model/dto/request/CityCreationRequest.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/request/CityCreationRequest.java
@@ -1,13 +1,4 @@
 package com.codecool.solarwatch.model.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class CityCreationRequest {
-    private String name;
-    private double longitude;
-    private double latitude;
-    private String country;
+public record CityCreationRequest(String name, double longitude, double latitude, String country) {
 }
--- backend/src/main/java/com/codecool/solarwatch/model/dto/request/CityModificationRequest.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/request/CityModificationRequest.java
@@ -1,13 +1,3 @@
 package com.codecool.solarwatch.model.dto.request;
 
-import lombok.Data;
-
-@Data
-public class CityModificationRequest {
-
-    private long id;
-    private String name;
-    private double longitude;
-    private double latitude;
-    private String country;
-}
+public record CityModificationRequest(long id, String name, double longitude, double latitude, String country) { }
